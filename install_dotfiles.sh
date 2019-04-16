5a6be1345f7ab5d6e30830d14f2800868d0090ec
Rendell Cale <rendellc@stud.ntnu.no>
2019-04-16 14:45:37 +0200

    Start xterm config

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # TODO: do this with a glob: needs to avoid .git/ .. . .gitignore
-DOTFILES="tmux.conf vimrc"
+DOTFILES="tmux.conf vimrc Xdefaults"
+SHDIR=$(realpath $(dirname $0))
 
 for file in $DOTFILES
 do
 	echo "linking $file to $HOME"
-	ln -sf $file $HOME/.$file
+	ln -sf $SHDIR/$file $HOME/\.$file
 done
