0638b11bf23c41eac46325dd3cc659ca616c7969
Rendell Cale <rendellc@stud.ntnu.no>
2019-04-21 13:29:42 +0200

    fix dotfiles script

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
 SHDIR=$(realpath $(dirname $0))
+CONFDIR=$(realpath $SHDIR/../configs)
 
 DOTFILES="tmux.conf vimrc"
 for file in $DOTFILES
 do
-	ln -sf $SHDIR/configs/$file $HOME/\.$file
+	ln -sf $CONFDIR/$file $HOME/\.$file
 done
 
--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 SHDIR=$(realpath $(dirname $0))
 
-ln -sf $SHDIR/configs/vimrc $HOME/\.vimrc
 $SHDIR/scripts/dotfiles.sh
 $SHDIR/scripts/uxterm.sh
 
