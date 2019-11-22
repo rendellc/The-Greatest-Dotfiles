c3d51f16e5bb761be86e5f2d7859a25050f10bf7
Rendell Cale <rendellc@stud.ntnu.no>
2019-11-22 16:55:02 +0100

    changes

--- update.sh
+++ update.sh
@@ -6,5 +6,6 @@ $SHDIR/scripts/uxterm.sh
 
 rm -rf $HOME/\.config/i3
 ln -sf $SHDIR/configs/i3 $HOME/\.config/i3
+ln -sf $SHDIR/configs/i3blocks $HOME/\.config/i3blocks
 ln -sf $SHDIR/configs/ranger $HOME/\.config/ranger
 ln -sf $SHDIR/configs/tmuxinator $HOME/\.tmuxinator
